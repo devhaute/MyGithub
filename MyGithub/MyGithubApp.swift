a54107e1da5aa95b82943a4f3579dbb1ce26ce47
devhaute <devhaute@gmail.com>
2024-01-13 01:20:42 +0900

    🌱 chore: 초기 환경 세팅

---
+++ MyGithub/MyGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGithubApp.swift
+//  MyGithub
+//
+//  Created by kai on 1/13/24.
+//
+
+import SwiftUI
+
+@main
+struct MyGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
