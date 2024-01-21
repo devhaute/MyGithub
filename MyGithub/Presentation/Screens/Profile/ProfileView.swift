93a7580be72778b537a0c5ef48afa035d27e0c3f
devhaute <devhaute@gmail.com>
2024-01-21 17:47:49 +0900

    feat: Profile View UI 구현

--- MyGithub/Presentation/Screens/Profile/ProfileView.swift
+++ MyGithub/Presentation/Screens/Profile/ProfileView.swift
@@ -7,16 +7,75 @@
 
 import SwiftUI
 import Factory
+import NukeUI
 
 struct ProfileView: View {
     @InjectedObject(\.profileViewModel) private var viewModel
     let userId: String
     
     var body: some View {
-        Text("Hello, World! \(viewModel.user?.id ?? "")!")
-            .onAppear {
-                viewModel.fetchUser(userId: userId)
+        VStack {
+            avatar
+            profile
+            safariLink
+        }
+        .onAppear {
+            viewModel.fetchUser(userId: userId)
+        }
+    }
+}
+
+extension ProfileView {
+    private var avatar: some View {
+        let avatarBorderlineWidth: CGFloat = 4
+        
+        return (
+            LazyImage(
+                source: viewModel.user?.avatarURL,
+                resizingMode: .aspectFit
+            )
+            .clipShape(Circle())
+            .overlay(
+                Circle()
+                    .stroke(
+                        LinearGradient(
+                            gradient: Gradient(colors: [Color.red, Color.yellow]),
+                            startPoint: .leading,
+                            endPoint: .trailing
+                        ),
+                        lineWidth: avatarBorderlineWidth).padding(-(avatarBorderlineWidth / 2))
+            )
+            .frame(width: 280, height: 280)
+        )
+    }
+    
+    private var profile: some View {
+        VStack {
+            Text(viewModel.user?.id ?? "")
+                .font(.title)
+            Text("🏠 \(viewModel.user?.location ?? "")")
+            Text(viewModel.user?.bio ?? "")
+            
+            HStack {
+                Text("Repos: \(viewModel.user?.publicRepos ?? 0)")
+                Text("Followers: \(viewModel.user?.followers ?? 0)")
+                Text("Following: \(viewModel.user?.following ?? 0)")
             }
+            .padding(.top, 10)
+        }
+    }
+    
+    @ViewBuilder
+    private var safariLink: some View {
+        if let htmlURL = URL(string: viewModel.user?.htmlURL ?? "") {
+            Link(destination: htmlURL) {
+                Text("웹페이지에서 보기")
+                    .padding(.top, 50)
+                    .font(.headline)
+            }
+        } else {
+            EmptyView()
+        }
     }
 }
 
